da0952a40924400ae4edf1175df04ba23e05e7cb
Akshit Yadav <akshit07032001@gmail.com>
2025-01-31 10:18:30 +0530

    commit

---
+++ httpserver-temp/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/sum', function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+
+  return res.status(200).json({ ans: a + b });
+});
+
+app.get('/multiply', function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+
+  return res.status(200).json({ ans: a * b });
+});
+
+app.get('/divide', function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+
+  return res.status(200).json({ ans: a / b });
+});
+
+app.get('/subtract', function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+
+  return res.status(200).json({ ans: a - b });
+});
+
+app.listen(3000);
