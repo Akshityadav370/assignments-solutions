a711d8ca8acbc0f214bed3eaacb7362495451528
Akshit Yadav <akshit07032001@gmail.com>
2025-03-15 13:42:28 +0530

    v2
    rooms added

--- ws-chat-app/dist/index.js
+++ ws-chat-app/dist/index.js
@@ -5,18 +5,27 @@ const wss = new ws_1.WebSocketServer({ port: 8080 });
 let usersConnected = 0;
 let allSockets = [];
 wss.on('connection', (socket) => {
-    allSockets.push(socket);
     usersConnected += 1;
     console.log('User Connected #' + usersConnected);
     socket.on('message', (e) => {
-        console.log(e.toString);
-        allSockets.forEach((s) => {
-            s.send(e.toString() + ':message sent from server!');
-        });
+        var _a;
+        // @ts-ignore
+        const parsedMessage = JSON.parse(e.toString());
+        if (parsedMessage.type === 'join') {
+            allSockets.push({ socket, room: parsedMessage.payload.roomId });
+        }
+        if (parsedMessage.type === 'chat') {
+            const currentUserRoom = (_a = allSockets.find((x) => x.socket === socket)) === null || _a === void 0 ? void 0 : _a.room;
+            const receivedMessage = parsedMessage.payload.message;
+            allSockets.forEach((user) => {
+                if (user.room === currentUserRoom) {
+                    user.socket.send(receivedMessage);
+                }
+            });
+        }
     });
     socket.on('close', () => {
         usersConnected -= 1;
         console.log('socket disconnected/closed #' + usersConnected);
-        allSockets = allSockets.filter((x) => x != socket);
     });
 });
--- ws-chat-app/src/index.ts
+++ ws-chat-app/src/index.ts
@@ -1,25 +1,40 @@
 import { WebSocketServer, WebSocket } from 'ws';
 
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
 const wss = new WebSocketServer({ port: 8080 });
 
 let usersConnected = 0;
-let allSockets: WebSocket[] = [];
+let allSockets: User[] = [];
 
 wss.on('connection', (socket) => {
-  allSockets.push(socket);
   usersConnected += 1;
   console.log('User Connected #' + usersConnected);
 
   socket.on('message', (e) => {
-    console.log(e.toString);
-    allSockets.forEach((s) => {
-      s.send(e.toString() + ':message sent from server!');
-    });
+    // @ts-ignore
+    const parsedMessage = JSON.parse(e.toString());
+    if (parsedMessage.type === 'join') {
+      allSockets.push({ socket, room: parsedMessage.payload.roomId });
+    }
+
+    if (parsedMessage.type === 'chat') {
+      const currentUserRoom = allSockets.find((x) => x.socket === socket)?.room;
+      const receivedMessage = parsedMessage.payload.message;
+
+      allSockets.forEach((user) => {
+        if (user.room === currentUserRoom) {
+          user.socket.send(receivedMessage);
+        }
+      });
+    }
   });
 
   socket.on('close', () => {
     usersConnected -= 1;
     console.log('socket disconnected/closed #' + usersConnected);
-    allSockets = allSockets.filter((x) => x != socket);
   });
 });
