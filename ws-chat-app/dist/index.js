f677b0cd086cdb9a2287cbad45a05237adb56a03
Akshit Yadav <akshit07032001@gmail.com>
2025-03-31 12:37:03 +0530

    refactor

--- ws-chat-app/dist/index.js
+++ ws-chat-app/dist/index.js
@@ -2,30 +2,43 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let usersConnected = 0;
-let allSockets = [];
+const rooms = {};
 wss.on('connection', (socket) => {
-    usersConnected += 1;
-    console.log('User Connected #' + usersConnected);
-    socket.on('message', (e) => {
+    console.log('New user connected');
+    let userRoom = null;
+    socket.on('message', (message) => {
         var _a;
-        // @ts-ignore
-        const parsedMessage = JSON.parse(e.toString());
-        if (parsedMessage.type === 'join') {
-            allSockets.push({ socket, room: parsedMessage.payload.roomId });
-        }
-        if (parsedMessage.type === 'chat') {
-            const currentUserRoom = (_a = allSockets.find((x) => x.socket === socket)) === null || _a === void 0 ? void 0 : _a.room;
-            const receivedMessage = parsedMessage.payload.message;
-            allSockets.forEach((user) => {
-                if (user.room === currentUserRoom) {
-                    user.socket.send(receivedMessage);
+        try {
+            const parsedMessage = JSON.parse(message.toString());
+            if (parsedMessage.type === 'join') {
+                const roomId = parsedMessage.payload.roomId;
+                userRoom = roomId;
+                if (!rooms[roomId]) {
+                    rooms[roomId] = new Set();
                 }
-            });
+                rooms[roomId].add(socket);
+            }
+            if (parsedMessage.type === 'chat' && userRoom) {
+                const receivedMessage = parsedMessage.payload.message;
+                (_a = rooms[userRoom]) === null || _a === void 0 ? void 0 : _a.forEach((client) => {
+                    if (client !== socket && client.readyState === ws_1.WebSocket.OPEN) {
+                        client.send(receivedMessage);
+                    }
+                });
+            }
+        }
+        catch (err) {
+            console.error('Invalid message format:', err);
         }
     });
     socket.on('close', () => {
-        usersConnected -= 1;
-        console.log('socket disconnected/closed #' + usersConnected);
+        if (userRoom && rooms[userRoom]) {
+            rooms[userRoom].delete(socket);
+            if (rooms[userRoom].size === 0) {
+                delete rooms[userRoom];
+            }
+        }
+        console.log('User disconnected');
     });
 });
+console.log('WebSocket server running on ws://localhost:8080');
