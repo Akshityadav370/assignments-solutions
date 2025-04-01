e7be3aee65db0ffc61eac0942b170a7cdc479cc6
Akshit Yadav <akshit07032001@gmail.com>
2025-04-01 21:51:31 +0530

    added zod validation

--- draw-app/apps/http-backend/src/routes/roomController/index.ts
+++ draw-app/apps/http-backend/src/routes/roomController/index.ts
@@ -1,9 +1,15 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '../../middleware/authMiddleware';
+import { CreateRoomSchema } from '@repo/common/types';
 
 const roomRouter: Router = express.Router();
 
 roomRouter.post('/create', authMiddleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    throw { status: 404, message: 'Invalid data' };
+  }
+
   // Logic to create a room
   res.status(200).json({ message: 'Room created successfully', roomId: 123 });
 });
--- draw-app/apps/http-backend/src/routes/userController/index.ts
+++ draw-app/apps/http-backend/src/routes/userController/index.ts
@@ -1,13 +1,27 @@
 import express, { Router } from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
+import { CreateUserSchema, LoginUserSchema } from '@repo/common/types';
 
 const userRouter: Router = express.Router();
 
-userRouter.post('/signup', (req, res) => {});
+userRouter.post('/signup', (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    throw { status: 404, message: 'Invalid data' };
+  }
+
+  res.json({ userId: 1 });
+});
 userRouter.post('/signin', (req, res) => {
+  const data = LoginUserSchema.safeParse(req.body);
+  if (!data.success) {
+    throw { status: 404, message: 'Invalid data' };
+  }
+
   const userId = 1;
   const token = jwt.sign({ userId }, JWT_SECRET);
+
   res.json({
     token,
   });
