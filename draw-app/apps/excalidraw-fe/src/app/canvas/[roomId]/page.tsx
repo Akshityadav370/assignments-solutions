c7874f2c82f5c57a2202e8befa0085453b7c4354
Akshit Yadav <akshit07032001@gmail.com>
2025-04-07 10:58:58 +0530

    restructure

--- draw-app/apps/excalidraw-fe/src/app/canvas/[roomId]/page.tsx
+++ draw-app/apps/excalidraw-fe/src/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { initDraw } from '@/draw';
 import { useEffect, useRef, useState } from 'react';
 
 const Canvas = () => {
@@ -14,44 +15,7 @@ const Canvas = () => {
     });
 
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      if (!ctx) return;
-
-      let clicked = false;
-      let startX = 0;
-      let startY = 0;
-
-      const handleMouseDown = (e: MouseEvent) => {
-        clicked = true;
-        startX = e.clientX;
-        startY = e.clientY;
-      };
-
-      const handleMouseUp = (e: MouseEvent) => {
-        clicked = false;
-        console.log(e.clientX, e.clientY);
-      };
-
-      const handleMouseMove = (e: MouseEvent) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-          ctx.strokeRect(startX, startY, width, height);
-        }
-      };
-
-      canvas.addEventListener('mousedown', handleMouseDown);
-      canvas.addEventListener('mouseup', handleMouseUp);
-      canvas.addEventListener('mousemove', handleMouseMove);
-
-      return () => {
-        canvas.removeEventListener('mousedown', handleMouseDown);
-        canvas.removeEventListener('mouseup', handleMouseUp);
-        canvas.removeEventListener('mousemove', handleMouseMove);
-      };
+      initDraw(canvasRef.current);
     }
   }, []);
 
