ef6d061a341800cd47b3ac8af235b1aed21d1454
Akshit Yadav <akshit07032001@gmail.com>
2025-04-10 20:15:10 +0530

    integrate db + http server

--- draw-app-practice-1/apps/http-server/src/controller/roomController.ts
+++ draw-app-practice-1/apps/http-server/src/controller/roomController.ts
@@ -1,22 +1,86 @@
-import express, { Request, Response, Router } from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import authMiddleWare, {
   AuthenticatedRequest,
 } from '../middleware/authMiddleware';
+import { prismaClient } from '@repo/db/client';
+import { CreateRoomSchema } from '@repo/common/types';
 
 const roomController: Router = express.Router();
+
+roomController.get(
+  '/chat/:roomId',
+  authMiddleWare,
+  async (req: Request, res: Response) => {
+    try {
+      const { roomId } = req.params;
+      const messages = await prismaClient.chat.findMany({
+        where: {
+          roomId: Number(roomId),
+        },
+        orderBy: {
+          id: 'desc',
+        },
+        take: 50,
+      });
+      res.json({ messages });
+    } catch (error) {
+      throw {
+        status: 500,
+        message: 'Error Fetching Messages from RoomId',
+      };
+    }
+  }
+);
 
 roomController.post(
   '/create',
   authMiddleWare,
-  (req: AuthenticatedRequest, res: Response) => {
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
-      const { name } = req.body;
-      // Create room
+      const parsedData = CreateRoomSchema.safeParse(req.body);
+      if (!parsedData.success) {
+        throw { status: 404, message: 'Invalid data' };
+      }
+      if (!req.userId) {
+        res.status(401).json({ message: 'User unauthorised!' });
+      }
+
+      const { name } = parsedData.data;
+
+      const room = await prismaClient.room.create({
+        data: {
+          slug: name,
+          admin: {
+            connect: {
+              id: req.userId,
+            },
+          },
+        },
+      });
 
       // Send room Id
-      res.json({ roomId: 2 });
+      res.json({ roomId: room.id, message: 'Room Created Succesfully!' });
     } catch (error) {
-      throw { status: 411, message: error };
+      next(error);
+    }
+  }
+);
+
+roomController.get(
+  '/:slug',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { slug } = req.params;
+      const room = await prismaClient.room.findFirst({ where: { slug } });
+
+      if (!room) {
+        throw { status: 201, message: 'Room not found!' };
+      }
+
+      res.json({ room });
+    } catch (error) {
+      console.error(error);
+      next(error);
     }
   }
 );
--- draw-app-practice-1/apps/http-server/src/controller/userController.ts
+++ draw-app-practice-1/apps/http-server/src/controller/userController.ts
@@ -1,32 +1,64 @@
-import express, { Request, Response, Router } from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
-import authMiddleWare from '../middleware/authMiddleware';
+import { CreateUserSchema, LoginUserSchema } from '@repo/common/types';
+import bcrypt from 'bcrypt';
+import { prismaClient } from '@repo/db/client';
 
 const userController: Router = express.Router();
 
-userController.post('/signin', (req: Request, res: Response) => {
+userController.post('/signin', async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body;
-    // Validate the zod schema
-    // Create user in DB
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+      throw new Error('Invalid data sent!');
+    }
+
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
+    const user = await prismaClient.user.create({
+      data: {
+        name: parsedData.data.name,
+        email: parsedData.data.email,
+        password: hashedPassword,
+      },
+    });
+
     res.json({ message: 'Signed up Successfully!' });
   } catch (error) {
     throw { status: 411, message: error };
   }
 });
 
-userController.post('/signin', (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
-    // Validate the zod schema
+userController.post(
+  '/signin',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const parsedData = LoginUserSchema.safeParse(req.body);
+      if (!parsedData.success) {
+        throw new Error('Invalid data sent!');
+      }
+      const { email, password } = parsedData.data;
+      const user = await prismaClient.user.findUnique({
+        where: { email },
+      });
 
-    const token = jwt.sign('1', JWT_SECRET);
+      if (!user) {
+        throw { status: 401, message: 'Invalid Credentials' };
+      }
 
-    res.json({ token });
-  } catch (error) {
-    throw { status: 411, message: error };
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        throw { status: 401, message: 'Invalid Credentials' };
+      }
+
+      const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+
+      res.json({ token });
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
   }
-});
+);
 
 export default userController;
