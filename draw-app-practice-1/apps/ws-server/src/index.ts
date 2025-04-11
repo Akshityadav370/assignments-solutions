5b31d1dd5ed9b022c49cf28bd7604edce95bc16f
Akshit Yadav <akshit07032001@gmail.com>
2025-04-11 05:35:19 +0530

    ws room logic

--- draw-app-practice-1/apps/ws-server/src/index.ts
+++ draw-app-practice-1/apps/ws-server/src/index.ts
@@ -1,38 +1,84 @@
-import { WebSocket, WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
+import { JOIN_ROOM, CHAT, LEAVE_ROOM } from '@repo/common/types';
+import { prismaClient } from '@repo/db/client';
+
+interface User {
+  ws: WebSocket;
+  rooms: string[];
+  userId: string;
+}
+
+const users = new Map<string, User>();
 
 const wss = new WebSocketServer({ port: 8080 });
 
-const validateUser = (token: string) => {
-  const decode = jwt.verify(token, JWT_SECRET);
-
-  if (decode && typeof decode === 'object' && 'userId' in decode) {
-    return decode.userId as string;
+function checkUser(token: string): string | null {
+  try {
+    const decode = jwt.verify(token, JWT_SECRET);
+    if (decode && typeof decode === 'object' && 'userId' in decode) {
+      return decode.userId as string;
+    }
+  } catch (error) {
+    console.log('Invalid token! Closing connection...');
   }
-
-  console.log('Invalid token! Closing Connection...');
   return null;
-};
+}
 
 wss.on('connection', (ws, req) => {
-  console.log('Client Connected!');
+  console.log('Client connected');
 
   const url = req.url;
-  if (!url) {
-    console.error('Please send the token');
-    return;
-  }
+  if (!url) return;
+
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || '';
-  const userId = validateUser(token);
+  const userId = checkUser(token);
 
-  if (userId == null) {
-    ws.close(404, 'Unauthorised!');
+  if (!userId) {
+    ws.close();
     return;
   }
 
+  users.set(userId, { userId, rooms: [], ws });
+
+  ws.on('message', async (message) => {
+    const parsedData = JSON.parse(message as unknown as string);
+    console.log('data', parsedData);
+
+    const user = users.get(userId);
+    if (!user) return;
+
+    if (parsedData.type === JOIN_ROOM) {
+      user.rooms.push(parsedData.roomId);
+    }
+
+    if (parsedData.type === LEAVE_ROOM) {
+      user.rooms = user.rooms.filter((x) => x !== parsedData.room);
+    }
+
+    if (parsedData.type === CHAT) {
+      const { roomId, message } = parsedData;
+
+      await prismaClient.chat.create({
+        data: {
+          roomId,
+          message,
+          userId,
+        },
+      });
+
+      for (const [, u] of users) {
+        if (u.rooms.includes(roomId)) {
+          u.ws.send(JSON.stringify({ type: CHAT, message, roomId }));
+        }
+      }
+    }
+  });
+
   ws.on('close', () => {
-    console.log('Client Disconnected!');
+    console.log('Client disconnected');
+    users.delete(userId);
   });
 });
