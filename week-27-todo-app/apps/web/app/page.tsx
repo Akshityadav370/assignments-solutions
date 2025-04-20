564085762babe6b934ae2d76241632ee0680308f
Akshit Yadav <akshit07032001@gmail.com>
2025-04-20 22:08:56 +0530

    docker files

--- week-27-todo-app/apps/web/app/page.tsx
+++ week-27-todo-app/apps/web/app/page.tsx
@@ -5,6 +5,7 @@ export default async function Home() {
   return <div>{JSON.stringify(users)}</div>;
 }
 
+// To make this page dynamic
 // export const revalidate = 60 // revalidate every 60 seconds
 // or
 // export const dynamic = 'force-dynamic'
